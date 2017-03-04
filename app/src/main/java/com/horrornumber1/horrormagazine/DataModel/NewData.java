8562d948d149e5ff7a1de451f82a7d6ebfc9dbb1
KIMTAEHO <hirooms2>
2017-03-04 14:42:53 +0900

    Initial commit

---
+++ app/src/main/java/com/horrornumber1/horrormagazine/DataModel/NewData.java
@@ -0,0 +1,35 @@
+package com.horrornumber1.horrormagazine.DataModel;
+
+/**
+ * Created by 김태호 on 2017-02-20.
+ */
+
+public class NewData {
+    String title;
+    String icon;
+    StringBuffer file;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public StringBuffer getFile() {
+        return file;
+    }
+
+    public void setFile(StringBuffer file) {
+        this.file = file;
+    }
+}
